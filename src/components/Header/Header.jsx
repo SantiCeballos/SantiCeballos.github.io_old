e042b8208197fbbbd1474eb56c586862dd9fdcb3
Santi Ceballos <santiceballosnavarro@gmail.com>
2024-04-08 15:04:08 +0200

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,15 +8,16 @@ function genRandomInt(max) {
 }
 
 export default function Header() {
-    const description = reactDescriptions[genRandomInt(2)];
-    return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-    );
-  }
+  const description = reactDescriptions[genRandomInt(2)];
+  return (
+    <header>
+      <a src="/src/help/index.hmtl">asdasdasd</a>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
