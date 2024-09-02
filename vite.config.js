7723b4f4d7e6c4fbc9d9e9c8a5193c615d559e33
santi ceballos <santiceballosnavarro@gmail.com>
2024-09-02 18:50:02 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -15,18 +15,6 @@ export default defineConfig({
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#4DBA87",
-        icons: [
-          {
-            src: "icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
       },
     }),
   ],
